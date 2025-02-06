224c33a1a6d0c10736a2c08cfb421966d6183444
nicolasmarinel <nicolas.marinel@gmail.com>
2025-02-05 21:24:22 -0600

    Minor changes in .gitignore.

---
+++ reload.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+cp -r /home/nico/Documents/PSM/testScene/ /home/nico/Documents/PSM/sceneTransfer/shooterFolder/files/
+sudo chmod -R 777 /home/nico/Documents/PSM/sceneTransfer/shooterFolder/files/testScene
